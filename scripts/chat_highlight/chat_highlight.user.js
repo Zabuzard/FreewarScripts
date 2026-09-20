38c7dc002fdbad24fa517612a45a0cc7f313cca3
Zabuzard <zabuza.dev@gmail.com>
2026-09-20 19:30:58 +0200

    chat highlight

--- scripts/chat_highlight/chat_highlight.user.js
+++ scripts/chat_highlight/chat_highlight.user.js
@@ -13,7 +13,8 @@
     /Massive Landqualle aktiviert ein magisches Schutzschild/i,
     /Behüter der Kathedrale zerbricht die Angriffswaffe/i,
     /flimmernde Farbanomalie richtet ihre vier Augen auf/i,
-    /nimmt das Item Briefbeschwerer durch einen Beutezauber auf/i
+    /nimmt das Item Briefbeschwerer durch einen Beutezauber auf/i,
+    /nimmt das Item Plan der Jerodar durch einen Beutezauber auf/i
   ];
 
   function highlightMessage(message) {
